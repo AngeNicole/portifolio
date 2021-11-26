87c6e1011c5802dfc59c740c4bed25883658e4e5
AngeNicole <angenicolemukundwa22@gmail.com>
2021-11-27 00:02:34 +0200

    Navbar Initialization

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import { RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import './navbar.css'
-import logo from '../../assets/assets/logo.svg'
+import logo from '../../assets/logo1.svg'
 
 const Navbar = () => {
     return (
-        <div className="gpt3__navbar">
+        // <div className="gpt3__navbar">
             <div className='gpt3__navbar-links'>
                 <div className="gpt3__navbar-links_logo">
                     <img src= {logo} alt="logo" />
                 </div>
                 <div className = "gpt3__navbar-links_container">
                     <p><a href="#home">Myself</a></p>
-                    <p><a href="#projects">Projects</a></p>
-                    <p><a href="#contact">Contact</a></p>
-                    <p><a href="#resume">Resume</a></p>
+                    <p><a href="#projects">02 Projects</a></p>
+                    <p><a href="#contact">03 Contact</a></p>
+                    <p><a href="#resume">04 Resume</a></p>
                 </div>
             </div>
-        </div>
+        // </div>
     )
 }
 
