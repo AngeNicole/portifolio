f2d8a8971a9218e78e4f3de1603211cc310fb00f
AngeNicole <angenicolemukundwa22@gmail.com>
2021-12-20 20:56:25 +0200

    Navbar

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './cta.css'
+import Navbar from '../navbar/Navbar'
+import logo from '../../assets/logo1.svg'
 
 const Cta = () => {
     return (
         <div className="center__rect">
-            
+             <div className="gpt3__navbar-links_container1">
+                    <p><a href="#myself">Myself</a></p>
+                    <p><a href="#contact">02 Contact</a></p>
+                </div>
+            <div className="gpt3__navbar-links_logo">
+                    <img src= {logo} alt="logo" />
+            </div>
+            <div className="gpt3__navbar-links_container2">
+                    <p><a href="#projects">04 Projects</a></p>
+                    <p><a href="#resume">05 Resume</a></p>
+                </div>
         </div>
     )
 }
--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,9 +1,11 @@
-// import twitter from '../../assets/icons/twitter.svg'
-// import behance from '../../assets/icons/behance.svg'
-// import dribbble from '../../assets/icons/dribbble.svg'
-// import github from '../../assets/icons/github.svg'
-// import instagram from '../../assets/icons/instagram.svg'
+import React, { useState } from 'react';
+import twitter from '../../assets/icons/twitter.svg'
+import behance from '../../assets/icons/behance.svg'
+import dribbble from '../../assets/icons/dribbble.svg'
+import github from '../../assets/icons/github.svg'
+import instagram from '../../assets/icons/instagram.svg'
 import Navbar from '../navbar/Navbar'
+import Cta from '../cta/CTA';
 import './feature.css'
 
 const feature = () => {
@@ -11,16 +13,15 @@ const feature = () => {
         
         <div className="feature__sidebar">
             <div className="feature__sidebar__links">
-                {/* <p><img src= {twitter} alt="" /></p>
+                <p><img src= {twitter} alt="" /></p>
                 <p><img src={behance} alt="" /></p>
                 <p><img src={dribbble} alt="" /></p>
                 <p><img src={github} alt="" /></p>
-                <p><img src= {instagram} alt="" /></p> */}
-                <p><a href="#twitter"><i class="si-instagram"></i></a></p>
+                <p><img src= {instagram} alt="" /></p>
             </div>
             <div className="feature__sidebar__line">
+                <Cta />
                 
-                <Navbar/>
             </div>
             
         </div>
